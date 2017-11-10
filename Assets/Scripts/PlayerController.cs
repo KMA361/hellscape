5c183962a6f54de8fda3a761f40c65566af9a311
Unknown <KMA361@nyu.edu>
2017-11-10 13:24:29 -0500

    file dump into new repo

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float speed = 800.0f;
+	public Vector3 jumpForce;
+
+	private Rigidbody _rigidbody;
+
+	void Start() {
+		jumpForce = new Vector3 (0.0f, 100.0f, 0.0f);
+		_rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		_rigidbody.AddForce (movement * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			_rigidbody.AddForce (jumpForce);
+		}
+	}
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Pickup")
+		{
+	other.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/playerdeath.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerdeath : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			SceneManager.LoadScene("gameoverscene");
+		}
+	
+		if (Input.GetKeyDown (KeyCode.R)) {
+			SceneManager.LoadScene ("MainScene");
+		}	
+	}
+}
